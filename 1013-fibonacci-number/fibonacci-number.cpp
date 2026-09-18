c4828ca4816a0618da3f0c9f008c54887883e7cc
POSANI NITHILAN <grensuke@gmail.com>
2026-09-18 11:35:22 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (19.73%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -6,7 +6,7 @@ public:
 
         if (dp[n] != -1) return dp[n];
 
-        return dp[n] = fibi(n-2)+fibi(n-1);
+        return dp[n] = fibi(n-1)+fibi(n-2);
     }
     int fib(int n) {
         dp.assign(n+1, -1);
